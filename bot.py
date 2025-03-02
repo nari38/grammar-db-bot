06a832b2269e231bb1d44228b9b74c37034b9ca6
Nariman <narimankazbekov57@gmail.com>
2025-03-02 14:33:07 +0500

    Добавил requirements.txt и Procfile

--- bot.py
+++ bot.py
@@ -113,5 +113,8 @@ def receive_update():
     
     return "OK"
 
+import os
+
 if __name__ == "__main__":
-    app.run(port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
